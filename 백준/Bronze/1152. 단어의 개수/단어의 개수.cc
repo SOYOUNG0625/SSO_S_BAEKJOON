62ce851fd7f255e9d660743d78ce9aca3bd4695f
SOYOUNG0625 <127066020+SOYOUNG0625@users.noreply.github.com>
2024-07-11 20:33:51 +0900

    [Bronze II] Title: 단어의 개수, Time: 40 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int count = 0;
+    string w;
+    while(cin >> w){
+        count++;
+    }
+    cout << count << endl;
+    return 0;
+}
