040b0fd7d327fec4ce5e7dad4c2fd74eecb2ed71
SOYOUNG0625 <127066020+SOYOUNG0625@users.noreply.github.com>
2024-11-22 22:25:29 +0900

    [level 1] Title: 모의고사, Time: 0.13 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,32 +1,31 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
+    vector<int> count(3, 0);
     int a[] = {1, 2, 3, 4, 5}, b[] = {2, 1, 2, 3, 2, 4, 2, 5}, c[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}, sa = 0, sb = 0, sc = 0;
     for(int i=0;i<answers.size();i++){
         if(a[i%5]==answers[i]){
-            sa++;
+            count[0]++;
         }
         if(b[i%8]==answers[i]){
-            sb++;
+            count[1]++;
         }
         if(c[i%10]==answers[i]){
-            sc++;
+            count[2]++;
         }
     }
-    int maxScore = max(sa, max(sb, sc));
+    int maxScore = *max_element(count.begin(), count.end());
 
-    if(sa == maxScore) {
-        answer.push_back(1);
+    for(int i=0;i<3;i++){
+        if(count[i] == maxScore) {
+            answer.push_back(i+1);
+        }
     }
-    if(sb == maxScore) {
-        answer.push_back(2);
-    }
-    if(sc == maxScore) {
-        answer.push_back(3);
-    }
+    
     return answer;
 }
