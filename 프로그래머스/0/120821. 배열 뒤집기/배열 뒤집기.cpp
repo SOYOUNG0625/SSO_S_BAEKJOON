5beca6b2ff80312330cc44abdeeb5fca0c5a68a8
SOYOUNG0625 <127066020+SOYOUNG0625@users.noreply.github.com>
2025-03-11 22:31:28 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 3.65 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list) {
     vector<int> answer;
-    for(int i=0;i<num_list.size();i++){
-        answer.push_back(num_list[num_list.size()-i-1]);
+    for(int i=num_list.size()-1;i>-1;i--){
+        answer.push_back(num_list[i]);
     }
     return answer;
 }
