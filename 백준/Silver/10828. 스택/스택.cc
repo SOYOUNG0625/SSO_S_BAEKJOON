83f0bb2fc932f9be3227629e8a3d9580f5ff5a7d
SOYOUNG0625 <127066020+SOYOUNG0625@users.noreply.github.com>
2025-03-21 12:56:18 +0900

    [Silver IV] Title: 스택, Time: 216 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ int main(){
+     int n;
+     cin >> n;
+     vector<int> stack;
+     int pos=0;
+     while(n--){
+         string s;
+         cin >> s;
+         if(s=="push"){
+             int num;
+             cin >> num;
+             stack.push_back(num);
+             pos++;
+         }else if(s=="pop"){
+             if(pos==0) cout << -1 << endl;
+             else{
+                 cout << stack[pos-1] << endl;
+                 stack.pop_back();
+                 pos--;
+             }
+         }else if(s=="size"){
+             cout << pos << endl;;
+         }else if(s=="empty"){
+             if(pos==0) cout << 1 << endl;
+             else cout << 0 << endl;
+         }else if(s=="top"){
+             if(pos==0) cout << -1 << endl;
+             else{
+                 cout << stack[pos-1] << endl;
+             }
+         }
+     }
+ }
