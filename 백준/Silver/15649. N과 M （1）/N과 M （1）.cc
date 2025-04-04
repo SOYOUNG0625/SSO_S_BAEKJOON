003cc602d76d2d4481bc432c31ea230b39bae813
SOYOUNG0625 <127066020+SOYOUNG0625@users.noreply.github.com>
2025-04-04 12:47:17 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> result;
+vector<bool> used;
+
+void make(int k) {
+    if (k == M) {
+        for (int num : result)
+            cout << num << " ";
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= N; ++i) {
+        if (!used[i]) {
+            used[i] = true;
+            result.push_back(i);
+            make(k + 1);
+            result.pop_back();   
+            used[i] = false;    
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    used.resize(N + 1, false);
+    make(0);
+    return 0;
+}
